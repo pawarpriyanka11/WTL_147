9772b35e4e123fdfd8f0b9e7f78c5cbe0ef74a9a
Priyanka Navnath Pawar <140964866+pawarpriyanka11@users.noreply.github.com>
2025-10-15 22:08:16 +0530

    Add files via upload

---
+++ ASSIGNMENTS/assign-7/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from "react";
+import Form1 from "./Form1";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Form Validation Demo</h1>
+      <Form1 />
+    </div>
+  );
+}
+
+export default App;
